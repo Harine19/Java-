7f749fbf6b19e82dec6ce113c01ea6a9feed1182
Harine Vidyasekaran <ch.en.u4cse21121@ch.students.amrita.edu>
2022-10-08 20:36:25 +0530

    Update and rename avgmultiple.java to Basic programs/6avgmultiple.java

--- Basic programs/6avgmultiple.java
+++ Basic programs/6avgmultiple.java
@@ -3,5 +3,8 @@ public class avgmultiple
   public static void main (String[] args)
   {
   int[] nums={1,2,3,4,5}; int sum=0;
-  sum=
+  sum=num1+num2;
+    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+  }
+}
    
